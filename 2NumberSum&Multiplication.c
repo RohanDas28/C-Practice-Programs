1f13c08311d1ba5f01faf2e8e9d6e2d73482b3a4
Rohan Das <42172455+RohanDas28@users.noreply.github.com>
2021-01-20 11:58:41 +0530

    Added 2NoSumMulti.c 🔥

---
+++ 2NumberSum&Multiplication.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+    int a,b,c,d;
+    printf("Enter the two numbers: ");
+    scanf("%d %d", &a,&b);
+    c=a+b;
+    d=a * b;
+    printf("The Sum and the Multiplication of the two numbers are %d, %d", c, d);
+
+
+}
