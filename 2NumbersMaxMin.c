13fa971e4f900c0598b81208e468b1127032327a
Rohan Das <rohandasbirbhum@gmail.com>
2021-01-20 23:00:58 +0530

    Added 2numbersmaxmin.c 🔥�

---
+++ 2NumbersMaxMin.c
@@ -0,0 +1,21 @@
+//Write a program to take 2 numbers and check 2 numbers are equal or maximum.
+#include<stdio.h>
+main(){
+    int a,b;
+    printf("Enter the two numbers: ");
+    scanf("%d %d", &a,&b);
+    if (a!=b){
+        if (a>b)
+    {
+        printf("A is Greater Than B");
+    }
+    else
+    {
+        printf("A Less Than B");  
+    }
+    }
+    else{
+        printf("A and B are the same");
+    }
+    
+}
