1be415c94fde5aa30ede0dbeff8f940cb4fcd7b3
Rohan Das <rohandasbirbhum@gmail.com>
2021-02-02 09:11:31 +0530

    Added GrossSalary.c✌

---
+++ grosssalary.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+    float basicSalary, DA, HRA, TA, grossSalary;
+    printf("Enter Basic Salary: ");
+    scanf("%f", &basicSalary);
+    DA = (basicSalary*15)/100;
+    HRA= (basicSalary*13)/100;
+    TA= (basicSalary*10)/100;
+    grossSalary= basicSalary+DA+HRA+TA;
+    printf("%f", grossSalary);
+}
