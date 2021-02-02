624b4d0819f1a50372a31eb9864cd4c2e70caf19
Rohan Das <42172455+RohanDas28@users.noreply.github.com>
2021-02-02 09:35:23 +0530

    Updated GrossSalary.c 🔥

--- grosssalary.c
+++ grosssalary.c
@@ -1,3 +1,4 @@
+//Write a program to take basic salary of an employee and calculate gross salary. DA is 15% of basic salary, HRA is 13% of Basic Salary and TA is 10% of basic salary.
 #include<stdio.h>
 main(){
     float basicSalary, DA, HRA, TA, grossSalary;
